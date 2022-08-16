50cb11c99db1c61d6970cf6f506af8b4cefededd
Derek WIllian <derekoob@hotmail.com>
2022-08-15 22:01:15 -0300

    Estrutura_Sequencial 11-18

---
+++ Pratica_1.py
@@ -0,0 +1,77 @@
+# 1 Faça um Programa que mostre a mensagem "Alo mundo" na tela. 
+
+# print("Alo mundo")
+
+# 2 Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número]. 
+
+#n = int(input("Insira um número: "))
+#print("O número informado foi",n)
+
+# 3 Faça um Programa que peça dois números e imprima a soma. 
+#n1 = int(input("Insira o primeiro valor: "))
+#n2 = int(input("Insira o segundo valor: "))
+#soma = n1 + n2
+#print(soma)
+
+# 4 Faça um Programa que peça as 4 notas bimestrais e mostre a média. 
+
+#n1 = int(input("Insira a primeira nota: "))
+#n2 = int(input("Insira a segunda nota: "))
+#n3 = int(input('Insira a terceira nota: '))
+#n4 = int(input('Insira a quarta nota: '))
+#media = (n1+n2+n3+n4)/4
+#print("A média é",media)
+
+# 5 Faça um Programa que converta metros para centímetros. 
+
+#n1 = float(input('Insira a quantidade em metros: '))
+#conv = n1*100
+#print('a conversão de metros para centimetros realizada é de:',conv)
+
+# 6 Faça um Programa que peça o raio de um círculo, calcule e mostre sua área. 
+
+#raio = float(input('Insira o raio do circulo: '))
+#formula = 3.14
+#calc = (raio**2)*formula
+#print ('A área do circulo é de:',calc)
+
+# 7 Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário. 
+
+#n1 = float(input('Insira a área do quadrado: '))
+#formula = (n1**2)*2
+#print('O dobro da área do quadrado é:',formula)
+
+# 8 Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês. 
+
+#n1 = float(input('Quanto você ganha por hora?: '))
+#n2 = float(input('Quantas horas você trabalha no mês: '))
+#calc = n1*n2
+#print ('O seu salário no mês é de :',calc)
+
+# 9 Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius. 
+
+#n1 = float(input('Insira a temperatura em Fahrenheit: '))
+#formula = (n1-32)*5/9
+#print('A temperatua em graus é de:',formula)
+
+# 10 Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit. 
+
+#n1 = float(input('Insira a temperatura em Celsius: '))
+#formula = (n1*9/5)+32
+#print('A temperatura em Fahrenheit é de:',formula)
+
+# 11 Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
+
+   # A. o produto do dobro do primeiro com metade do segundo .
+   # B. a soma do triplo do primeiro com o terceiro.
+   # C. o terceiro elevado ao cubo. 
+
+n1 = int(input('Insira um valor inteiro: '))
+n2 = int(input('Insira um segundo valor inteiro: '))
+n3 = float(input('Insira um valor real: '))
+
+a = (n1*2)+n2/2
+b = (n1*3)+n3
+c = (n3**3)
+
+print(a,b,c)
